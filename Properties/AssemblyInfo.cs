9af363f19238f3136b710711cf976f5dbee98c28
Andy White <andy@soundintheory.co.uk>
2016-02-10 13:24:40 +0000

    Version bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Squiggle Api .NET")]
+[assembly: AssemblyTitle("Squiggle Api")]
 [assembly: AssemblyDescription("C# wrapper for the Squiggle API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Occam Software")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyFileVersion("0.1.2")]
